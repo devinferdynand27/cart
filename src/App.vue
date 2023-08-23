2cca63b5640337b59df7bb4f70f809177033e9a5
devinferdynandjobs <jobsdevin2@gmail.com>
2023-08-23 11:46:12 +0700

    cart

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
   </div>
 </template>
 <script>
-import navbarcomponent from '@/components/Navbarcomponent.vue';
+import navbarcomponent from '@/components/NavbarComponent.vue';
 import SubNavbarComponent from '@/components/SubNavbarComponent.vue';
 import  FooterComponent  from '@/components/FooterComponent.vue';
 export default {
